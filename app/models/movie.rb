1e15c62125a825e88a58fe5bb5c086592ace9316
Michael <michael.w.kolb@gmail.com>
2022-07-28 15:26:30 -0400

    commit tree ee20a39eed948188b021fb6dbac8220e9b2ab966

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ActiveRecord::Base
-
+    def self.create_with_title(title)
+        current_movie = Movie.new()
+        current_movie.title = title
+        current_movie.title.save
+    end
 end
